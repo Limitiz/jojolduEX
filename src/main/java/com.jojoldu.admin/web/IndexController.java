0ee71c5d24f6ba62827f9fe613d392d020de4d0e
Limitiz <ll55kk@naver.com>
2021-02-24 18:22:39 +0900

    mustache view

---
+++ src/main/java/com.jojoldu.admin/web/IndexController.java
@@ -0,0 +1,12 @@
+package com.jojoldu.admin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "src/main/resources/template/index.mustache";
+    }
+}
